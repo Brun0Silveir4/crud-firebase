a689707b3de41952f56abff485b850dab0501d1c
BrunoSilveiraEtecZl <bruno.dionisio@fatec.sp.gov.br>
2023-06-30 13:38:14 -0300

    Crud completo

--- app.js
+++ app.js
@@ -26,15 +26,35 @@ app.get("/", function(req, res){
 })
 
 app.get("/consulta", function(req, res){
-    
+  db.collection('agendamentos').get().then((snapshot) => {
+    const agendamentos = [];
+    snapshot.forEach((doc) => {
+      agendamentos.push({ id: doc.id, ...doc.data() });
+    });
+    res.render("consulta", { agendamentos });
+  })
 })
 
 app.get("/editar/:id", function(req, res){
-    
+  const id = req.params.id;
+  db.collection('agendamentos').doc(id).get().then((doc) => {
+      if (doc.exists) {
+        const agendamentoData = doc.data();
+        agendamentoData.id = id
+        console.log(agendamentoData)
+        res.render("editar", { agendamento: agendamentoData });
+      } else {
+          res.render("editar", { error: 'Dado inválido.' });
+      }
+  })
 })
 
 app.get("/excluir/:id", function(req, res){
-   
+  const id = req.params.id;
+  db.collection('agendamentos').doc(id).delete().then(() => {
+      console.log('Dado removido com sucesso.');
+      res.redirect('/consulta');
+  })
 })
 
 app.post("/cadastrar", function(req, res){
@@ -48,10 +68,20 @@ var result = db.collection('agendamentos').add({
   });
   
   console.log('Added document');
+  res.redirect('/');
 })
 
 app.post("/atualizar", function(req, res){
-    
+  const id = req.body.id;
+  db.collection('agendamentos').doc(id).update({
+    nome: req.body.nome,
+    telefone: req.body.telefone,
+    origem: req.body.origem,
+    data_contato: req.body.data_contato,
+    observacao: req.body.observacao
+  })
+  console.log('O dado foi atualizado.');
+  res.redirect('/consulta');
 })
 
 app.listen(8081, function(){
